ef7459465f8174d88a652963018696246592e068
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-15 16:50:31 -0600

    finished the little project

--- code.js
+++ code.js
@@ -9,19 +9,37 @@ getH1("Hola ando cambiando un  h1 con JS")
 
 function manipularP(texto) {
     var div = document.getElementsByTagName('div')[0]
+    console.log(div)
     var p = div.children[0]
     p.textContent = texto
-    console.log(div)
 }
 
 manipularP("Hola ando poniendo texto en p dentro de un div")
 
 function imagenLink(link) {
     var img = document.getElementsByTagName('img')[0]
+    console.log(img)
     var src = img.attributes[0]
     src.value = link
-    console.log(img)
 }
 
 imagenLink("")
 
+function estilo(color) {
+    var h2 = document.getElementsByTagName('h2')[0]
+    console.log(h2)
+    var style = h2.attributes[0]
+    style.value = "color:" + color + ";"
+}
+
+estilo("red")
+
+function creaP(texto) {
+    var div = document.getElementsByTagName('div')[1]
+    console.log(div)
+    var p = document.createElement('p')
+    div.appendChild(p)
+    div.children[0].textContent = texto
+}
+
+creaP("HOLA!!!!!!!!!!")
